2e9699239806e6748e053ca867a1f239c90ca356
buraksenyurt <burakselimsenyurt@gmail.com>
2024-08-19 21:02:51 +0300

    Applied resistance pack to test app

--- src/Resistance.TestApi/Program.cs
+++ src/Resistance.TestApi/Program.cs
@@ -1,7 +1,15 @@
+using Resistance;
+using Resistance.Behavior.Inconsistency;
+using Resistance.Behavior.Latency;
+using Resistance.Behavior.NetworkFailure;
+using Resistance.Behavior.Outage;
+using Resistance.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<ResistanceFlags>(builder.Configuration.GetSection("ResistanceFlags"));
 
 var app = builder.Build();
 
@@ -12,6 +20,28 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseResistance(new ResistanceOptions
+{
+    // Network Failure (HTTP 500 Internal Service Error with %25 probility)
+    NetworkFailureProbability = NetworkFailureProbability.Percent25,
+    // Produce HTTP 429 Too Many Request scenario with 3 concurrent request
+    ResourceRaceUpperLimit = 3,
+    // Manipulating response data with %50 probability
+    DataInconsistencyProbability = DataInconsistencyProbability.Percent20,
+    // Produce HTTP 503 Service Unavailable 10 seconds per minute
+    OutagePeriod = new OutagePeriod
+    {
+        Duration = TimeSpan.FromSeconds(10),
+        Frequency = TimeSpan.FromMinutes(1)
+    },
+    // Latency 500 millisecdons - 2500 milliseconds
+    LatencyPeriod = new LatencyPeriod
+    {
+        MinDelayMs = TimeSpan.FromMilliseconds(500),
+        MaxDelayMs = TimeSpan.FromMilliseconds(2500)
+    }
+});
 
 app.MapGet("/getSalary", () =>
 {
