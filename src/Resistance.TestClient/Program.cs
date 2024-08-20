a05457a239e1e9011008baa34d9111a6eb265e18
buraksenyurt <burakselimsenyurt@gmail.com>
2024-08-20 20:44:03 +0300

    Added a test client app

---
+++ src/Resistance.TestClient/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Press any key to start test");
+Console.ReadLine();
+Console.WriteLine("Running...");
+Console.WriteLine("To stop CTRL + C");
+
+using HttpClient client = new() { BaseAddress = new Uri("http://localhost:5225/") };
+
+while (true)
+{
+    var response = await client.GetAsync("getSalary");
+    Console.WriteLine($"{response.StatusCode} - {await response.Content.ReadAsStringAsync()}");
+    Task.Delay(500).Wait();
+}
